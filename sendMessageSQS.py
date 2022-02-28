30df8143a56bb2ebbe60b37407fce295f79a2755
Mark Robinson <mhrobinson123@gmail.com>
2022-02-27 19:35:51 -0500

    Update sendMessageSQS.py

--- sendMessageSQS.py
+++ sendMessageSQS.py
@@ -1,3 +1,19 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+
+    sqsURL = "SQS URL"
+    message = "SQS String Message"
+
+    sendMessageSQS(sqsURL, message)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
 def sendMessageSQS(sqsURL, message):
 
     sqs = boto3.client('sqs')
